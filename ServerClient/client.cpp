402b4d88440b8d16d531515d2f96c6fe6242b466
Bartosz Pajewski <bartoszpajewski@gmail.com>
2019-08-28 13:12:26 +0200

    Slots and signals
    Slots and signals for management socket.

--- ServerClient/client.cpp
+++ ServerClient/client.cpp
@@ -4,3 +4,22 @@ Client::Client(QObject *parent) : QObject(parent)
 {
 
 }
+
+void Client::connect()
+{
+    _socket = new QTcpSocket(this);
+
+    QObject::connect(_socket,&QTcpSocket::connected,this,&Client::_connected);
+    QObject::connect(_socket,&QTcpSocket::disconnected,this,&Client::_disconnected);
+    QObject::connect(_socket,&QIODevice::readyRead, this, &Client::_read);
+    QObject::connect(_socket, QOverload<QAbstractSocket::SocketError>::of(&QAbstractSocket::error),this,&Client::_error);
+    //connect(socket,SIGNAL(readyRead()),this,SLOT(readyRead()));
+
+    _socket->connectToHost(_settings.host,_settings.port);
+}
+
+void Client::_read()
+{
+    _stream.startTransaction();
+
+}
--- ServerClient/client.hpp
+++ ServerClient/client.hpp
@@ -1,6 +1,11 @@
 #ifndef CLIENT_HPP
 #define CLIENT_HPP
 
+#include <QtNetwork/QTcpSocket>
+#include <QtNetwork/QAbstractSocket>
+
+#include <QDataStream>
+
 #include <QObject>
 
 class Client : public QObject
@@ -9,9 +14,35 @@ class Client : public QObject
 public:
     explicit Client(QObject *parent = nullptr);
 
+    void connect();
+
 signals:
 
+    void connected();
+    void disconnected();
+
 public slots:
+
+private slots:
+
+    void _connected();
+    void _disconnected();
+    void _read();
+    void _error(QAbstractSocket::SocketError socketError);
+
+
+private:
+
+    QTcpSocket *_socket {nullptr};
+    QDataStream _stream;
+
+    struct connectionSettings
+    {
+        QString host = "127.0.0.1";
+        quint16 port = 3456;
+    };
+
+    connectionSettings _settings;
 };
 
 #endif // CLIENT_HPP
